4d5f75551a17c9772a0cdb5c4de5bb26779de99c
AqibMirza91 <A.H.Mirza@kingston.ac.uk>
2022-04-21 15:37:07 +0100

    + Added VLC DLL's to project for interoperability
    + Linked up the VLC code in GetVideo.cs and VLCWrapper

---
+++ Assets/GetVideo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetVideo : MonoBehaviour
+{
+
+    public UnityWebRequest uwr;
+    public VLCWrapper VLCinstance;
+
+    void Start()
+    {
+        VLCWrapper.Test.Run();
+        RTSPRequest();
+    }
+
+    public void RTSPRequest()
+    {
+        uwr.url = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4";
+    }
+}
