c25f5aea99be0107b9f859049ddd8f5505664391
AqibMirza91 <A.H.Mirza@kingston.ac.uk>
2022-09-01 13:57:34 +0100

    Code cleanup + Importing of HL7 FHIR PKG
    + Added Nuget for Unity Package also

--- Assets/Scripts/VideoPlayerControls.cs
+++ Assets/Scripts/VideoPlayerControls.cs
@@ -5,39 +5,10 @@ using UnityEngine.Video;
 
 public class VideoPlayerControls : MonoBehaviour
 {
-    public GameObject PlayButton;
-    public GameObject PauseButton;
-    public GameObject RewindButton;
-    public GameObject ForwardButton;
     public GameObject ExitButton;
-
-    public VideoPlayer videoPlayer;
     public VideoPlayer vitalSigns;
-    private float speed = 10f;
 
     public Camera PiPCamera;
-
-    public void Play()
-    {
-        videoPlayer.Play();
-        vitalSigns.Play();
-    }
-
-    public void Pause()
-    {
-        videoPlayer.Pause();
-        vitalSigns.Pause();
-    }
-
-    public void Rewind()
-    {
-        videoPlayer.time -= speed;
-    }
-
-    public void Forward()
-    {
-        videoPlayer.time += speed;
-    }
 
     public void Exit()
     {
