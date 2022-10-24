e47667b09d01999bc03d4cd224148b4b3088e5cc
AqibMirza91 <A.H.Mirza@kingston.ac.uk>
2022-10-24 13:11:49 +0100

    Multiple Project Changes
    + Sorted and removed outdated packages
    
    + Removed old scripts
    
    + Took away UniSave Pkg for time being

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,13 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
-using Unity.Services.Relay;
-using Unity.Services.Relay.Models;
 using System;
-using System.Linq;
-using Unity.Services.Core;
-using System.Threading;
 
 public class SceneManager : MonoBehaviour
 {
@@ -15,13 +9,6 @@ public class SceneManager : MonoBehaviour
     public GameObject[] SceneGO1 = new GameObject[7];
     public GameObject vitalSigns;
     public GameObject videoRelay;
-    public void Testing()
-    {
-        Debug.Log(MindrayConnect.GetLocalIPv4());
-        MindrayConnect.GetLocalIPv4();
-        MindrayConnect.ConnectviaTCP();
-    }
-
     public void VideoFullScreen()
     {
         for (int i = 0; i < SceneGO1.Length; i++)
